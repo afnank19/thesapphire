b3bbc194dc49763f96778c25e106f296fd5e7db9
Afnan Khalid <afnank19@gmail.com>
2024-10-07 18:57:49 +0500

    add prettier config, add tailwindcss for vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-    return (
-        <>
-            <p>Aces</p>
-        </>
-    );
+  return (
+    <>
+      <div className="container mx-auto text-center">
+        <p className="text-3xl underline">Aces</p>
+      </div>
+    </>
+  );
 }
 
 export default App;
