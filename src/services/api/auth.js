6f866c06845091846e020540e4b7c7451cc7b1be
Afnan Khalid <afnank19@gmail.com>
2024-12-19 18:30:30 +0500

    feat(auth): Add localStorage based refresh token flow, fix styling for image gallery, fix category bug in homefeed

--- src/services/api/auth.js
+++ src/services/api/auth.js
@@ -12,16 +12,21 @@ export const signIn = async ({ email, password }) => {
     }
   );
 
+  localStorage.setItem('__r_tkn', response.data.rTkn);
   return response.data;
 };
 
 export const getSession = async () => {
+  const rToken = localStorage.getItem('__r_tkn');
+
   const response = await noAuthInstance.get('/session', {
     withCredentials: true,
     headers: {
-      'X-1TIME-TKN': 'lou'
+      'X-1TIME-TKN': 'lou',
+      Authorization: `Bearer ${rToken}`
     }
   });
 
+  localStorage.setItem('__r_tkn', response.data.rTkn);
   return response.data;
 };
